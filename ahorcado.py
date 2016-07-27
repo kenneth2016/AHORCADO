cc1a2f42477d26e0dc83a6e7e148f7b436d9f222
kenneth2016 <kennethl1801@gmail.com>
2016-07-27 08:43:32 -0600

    mejoras hehehe

--- ahorcado.py
+++ ahorcado.py
@@ -2,22 +2,25 @@ import funciones
 print ("BIENVENIDO AL AHORCADO SUPREMO!!!!")
 errores=0
 l1=[]
+pa=[]
 p=funciones.palabras(l1)
 print(p)
-print("_ "*len(p))
-pa= list(p)
-res = input("ingrese una letra: ")
+for letra in p:
+	pa.append("_")
+while errores <= 7:
+	errores=0
+	while "_" in pa:
+		oculto= " ".join(pa)
+		print (oculto)
 
-if res in pa:
-	print("si")
-else:	
-	print("_ _ _ _ _")
-	print("|")
-	print("|")
-	print("|")
-	print("|")
-	print("|")
+		res = input("ingrese una letra: ")
+		encontrado=False
+		for i in range(0, len(p)):
 
-
-
-
+			if p[i] == res:
+				pa[i]=res
+		if res not in pa:
+			errores=errores+1
+print(p) 
+print ("gracias por jugar")
+	
